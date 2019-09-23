c79bfc6d5123a37659a5ecc9d22619f0f5df66fd
Helge Heß <devteam@zeezide.de>
2019-09-23 18:50:00 +0200

    Disable async to-one fetch, was wrong
    ... we need to stay on the main queue.

--- Sources/DirectToSwiftUI/ViewModel/D2SToOneFetch.swift
+++ Sources/DirectToSwiftUI/ViewModel/D2SToOneFetch.swift
@@ -23,30 +23,9 @@ public final class D2SToOneFetch: ObservableObject {
   public init(object: NSManagedObject, propertyKey: String) {
     self.object      = object
     self.propertyKey = propertyKey
-    
-    func lookup(_ kp: String, in object: NSManagedObject) -> NSManagedObject? {
-      KeyValueCoding.value(forKeyPath: kp, inObject: object)
-        as? NSManagedObject
-    }
-    
-    self.destination = lookup(propertyKey, in: object)
+    self.destination = object.value(forKeyPath: propertyKey) as? NSManagedObject
   }
   
   func resume() {
-    guard destination == nil else { return }
-    
-    // Note: This _does_ try to fetch `nil` values, which is not strictly
-    //       wrong.
-    
-    _ = object.fetchToOneRelationship(propertyKey)
-      .receive(on: RunLoop.main) // this keeps it around
-      .catch { ( error : Swift.Error ) -> Just<NSManagedObject?> in
-        return Just(nil)
-      }
-      .sink { newValue in
-        // Update the AR. This is a little meh, but avoids continuous fetches.
-        self.object.setValue(newValue, forKeyPath: self.propertyKey)
-        self.destination = newValue
-      }
   }
 }
